ea1a616fb92042711720373eb83868ca77a76c0a
papuala <pauladiazcarreras1b@gmail.com>
2022-11-15 22:55:33 +0100

    home menu creation and more

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
-const name ='world';
-console.log(`Hello ${name}!`);
+function openNav() {
+    document.getElementById("home-menu").style.display = "none";
+    document.getElementById("close-home-menu").style.display = "block";
+    document.getElementsByTagName("nav")[0].style.display = "flex";
+    document.getElementsByTagName("h1")[0].style.display = "none";
+}
+function closeNav() {
+    document.getElementById("home-menu").style.display = "block";
+    document.getElementById("close-home-menu").style.display = "none";
+    document.getElementsByTagName("nav")[0].style.display = "none";
+    document.getElementsByTagName("h1")[0].style.display = "block";
+}
