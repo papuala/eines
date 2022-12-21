43d01cf31818789d75b80d2057c39619cff0c197
papuala <pauladiazcarreras1b@gmail.com>
2022-12-21 18:08:13 +0100

    clip-path and svg animation

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ function openNav() {
     document.getElementById("close-home-menu").style.display = "block";
     document.getElementsByTagName("nav")[0].style.display = "flex";
     document.getElementsByTagName("h1")[0].style.display = "none";
+    document.getElementsByTagName("main")[0].style.display = "none";
 }
 function closeNav() {
     document.getElementById("home-menu").style.display = "block";
     document.getElementById("close-home-menu").style.display = "none";
     document.getElementsByTagName("nav")[0].style.display = "none";
     document.getElementsByTagName("h1")[0].style.display = "block";
+    document.getElementsByTagName("main")[0].style.display = "block";
 }
