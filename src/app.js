96a4479d6e73c7d9172f80b36b1805fa08038657
papuala <pauladiazcarreras1b@gmail.com>
2023-01-21 13:10:18 +0100

    carrega asincrona

--- src/app.js
+++ src/app.js
@@ -12,19 +12,4 @@ function closeNav() {
     document.getElementsByTagName("h1")[0].style.display = "block";
     document.getElementsByTagName("main")[0].style.display = "block";
 }
-document.addEventListener("DOMContentLoaded", function() {
-  var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-background"));
-  if ("IntersectionObserver" in window) {
-    let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("visible");
-          lazyBackgroundObserver.unobserve(entry.target);
-        }
-      });
-    });
-    lazyBackgrounds.forEach(function(lazyBackground) {
-      lazyBackgroundObserver.observe(lazyBackground);
-    });
-  }
-});
+
